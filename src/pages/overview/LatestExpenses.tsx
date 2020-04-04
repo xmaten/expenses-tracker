0d314b3722a8ae46ff4a382d76f1981af75e42a2
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-04-04 14:09:29 +0200

    Remove not needed import

--- src/pages/overview/LatestExpenses.tsx
+++ src/pages/overview/LatestExpenses.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col, Typography, Avatar } from 'antd'
 
-const { Title, Paragraph, Text } = Typography
+const { Title, Paragraph } = Typography
 
 const expensesList = [
   {
