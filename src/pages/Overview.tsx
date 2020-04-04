a11b43d81cad1838773596d81ce6a8d19b1c06fa
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-04-04 13:34:13 +0200

    Create basic navigation

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Layout } from 'antd'
 
+import { Nav } from 'components/nav/Nav'
+
 const { Header, Sider, Content } = Layout
 
 export const Overview = () => (
   <Layout>
-    <Header>Header</Header>
+    <Header>
+      <Nav />
+    </Header>
     <Layout>
       <Content>Content</Content>
       <Sider>Sider</Sider>
