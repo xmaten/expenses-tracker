9ed582a66ca51403792e534d9d3f0b88fa367e6a
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-04-11 14:01:18 +0200

    Create httpClient

---
+++ src/utils/httpClient.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const httpClient = () => {
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || '',
+  })
+
+  return instance
+}
+
+export default httpClient()
