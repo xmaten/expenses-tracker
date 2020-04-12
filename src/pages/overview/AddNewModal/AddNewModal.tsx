8265330c0db29a28300d78c3d32c218ca2fe390e
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-04-12 18:27:09 +0200

    Create form handlers

--- src/pages/overview/AddNewModal/AddNewModal.tsx
+++ src/pages/overview/AddNewModal/AddNewModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal, Row, Col, Typography, Select, Input, InputNumber } from 'antd'
 
 type Props = {
@@ -11,41 +11,80 @@ type Props = {
 const { Title } = Typography
 const { Option } = Select
 
-export const AddNewModal: React.FC<Props> = ({ title, isVisible, onOk, onCancel }) => (
-  <Modal
-    title={title}
-    visible={isVisible}
-    onOk={() => onOk(false)}
-    onCancel={() => onCancel(false)}
-  >
-    <Row type="flex" justify="space-between">
-      <Col span={12}>
-        <Title level={3}>Type</Title>
-        <Select defaultValue="expense" style={{ width: '100%' }}>
-          <Option value="expense">Expense</Option>
-          <Option value="revenue">Revenue</Option>
-        </Select>
-      </Col>
-      <Col span={11}>
-        <Title level={3}>Category</Title>
-        <Select defaultValue="electronics" style={{ width: '100%' }}>
-          <Option value="electronics">Electronics</Option>
-          <Option value="credit">Credit</Option>
-          <Option value="food">Food</Option>
-          <Option value="entertainment">Entertainment</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Col>
-    </Row>
-    <Row style={{ marginTop: '50px' }} type="flex" justify="space-between">
-      <Col span={12}>
-        <Title level={3}>Name</Title>
-        <Input placeholder="Type here" />
-      </Col>
-      <Col span={11}>
-        <Title level={3}>Value</Title>
-        <InputNumber placeholder="Type here" style={{ width: '100%' }} />
-      </Col>
-    </Row>
-  </Modal>
-)
+export const AddNewModal: React.FC<Props> = ({ title, isVisible, onOk, onCancel }) => {
+  const [nameVal, setNameVal] = useState('')
+  const [typeVal, setTypeVal] = useState('expense')
+  const [valueVal, setValueVal] = useState<number | undefined>(undefined)
+  const [categoryVal, setCategoryVal] = useState('food')
+
+  const onSubmit = () => {
+    const formData = {
+      name: nameVal,
+      type: typeVal,
+      value: valueVal,
+      category: categoryVal,
+    }
+
+    console.log(formData)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Modal
+        title={title}
+        visible={isVisible}
+        onOk={() => onSubmit()}
+        onCancel={() => onCancel(false)}
+      >
+        <Row type="flex" justify="space-between">
+          <Col span={12}>
+            <Title level={3}>Type</Title>
+            <Select
+              defaultValue={typeVal}
+              onChange={(val: string) => setTypeVal(val)}
+              style={{ width: '100%' }}
+            >
+              <Option value="expense">Expense</Option>
+              <Option value="revenue">Revenue</Option>
+            </Select>
+          </Col>
+          <Col span={11}>
+            <Title level={3}>Category</Title>
+            <Select
+              defaultValue={categoryVal}
+              onChange={(val: string) => setCategoryVal(val)}
+              style={{ width: '100%' }}
+            >
+              <Option value="electronics">Electronics</Option>
+              <Option value="credit">Credit</Option>
+              <Option value="food">Food</Option>
+              <Option value="entertainment">Entertainment</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row style={{ marginTop: '50px' }} type="flex" justify="space-between">
+          <Col span={12}>
+            <Title level={3}>Name</Title>
+            <Input
+              placeholder="Type here"
+              name="name"
+              value={nameVal}
+              onChange={(e) => setNameVal(e.target.value)}
+            />
+          </Col>
+          <Col span={11}>
+            <Title level={3}>Value</Title>
+            <InputNumber
+              placeholder="Type here"
+              style={{ width: '100%' }}
+              name="value"
+              value={valueVal}
+              onChange={(val) => setValueVal(val)}
+            />
+          </Col>
+        </Row>
+      </Modal>
+    </form>
+  )
+}
