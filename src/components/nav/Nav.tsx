83ad1150e5cbd262993ab386128ff64d98f33b0a
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-05-10 11:53:53 +0200

    Add ability to logout

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,28 +2,40 @@ import React from 'react'
 import { Menu, Row, Col, Typography } from 'antd'
 import { NavLink } from 'react-router-dom'
 
+import { removeFromStorage } from 'utils/localStorage'
+
 const { Title } = Typography
 
-export const Nav = () => (
-  <Row align="middle" style={{ height: '100%' }}>
-    <Col span={16}>
-      <Title level={3} style={{ marginBottom: 0, color: '#fff' }}>
-        ExpensesTracker
-      </Title>
-    </Col>
+export const Nav = () => {
+  const logout = () => {
+    removeFromStorage('access-token')
+    window.location.reload()
+  }
 
-    <Col span={8}>
-      <Menu mode="horizontal" theme="dark">
-        <Menu.Item>
-          <NavLink to="/">Overview</NavLink>
-        </Menu.Item>
-        <Menu.Item>
-          <NavLink to="/calendar">Calendar</NavLink>
-        </Menu.Item>
-        <Menu.Item>
-          <NavLink to="/reports">Reports</NavLink>
-        </Menu.Item>
-      </Menu>
-    </Col>
-  </Row>
-)
+  return (
+    <Row align="middle" style={{ height: '100%' }}>
+      <Col span={16}>
+        <Title level={3} style={{ marginBottom: 0, color: '#fff' }}>
+          ExpensesTracker
+        </Title>
+      </Col>
+
+      <Col span={8}>
+        <Menu mode="horizontal" theme="dark">
+          <Menu.Item>
+            <NavLink to="/">Overview</NavLink>
+          </Menu.Item>
+          <Menu.Item>
+            <NavLink to="/calendar">Calendar</NavLink>
+          </Menu.Item>
+          <Menu.Item>
+            <NavLink to="/reports">Reports</NavLink>
+          </Menu.Item>
+          <Menu.Item>
+            <p onClick={logout}>Logout</p>
+          </Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
+  )
+}
