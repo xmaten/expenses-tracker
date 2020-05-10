f34cf198b9edfc9d87d1a965f37cc5917001ff0e
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-05-10 11:58:11 +0200

    Change httpClient to use access token on request

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,9 +1,43 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 
-const httpClient = () => {
-  const instance = axios.create({
+import { getFromStorage, removeFromStorage } from './localStorage'
+
+export function setRequestAuthHeaders(headers: { [key: string]: string }) {
+  const value = getFromStorage('access-token')
+
+  if (value) {
+    headers['Authorization'] = `Bearer ${value}`
+  }
+}
+
+export function onRequest(config: AxiosRequestConfig): AxiosRequestConfig {
+  setRequestAuthHeaders(config.headers)
+
+  return config
+}
+
+function onResponseSuccess(response: AxiosResponse): AxiosResponse {
+  return response
+}
+
+export function onResponseError(error: AxiosError): Promise<never> {
+  if (error.response?.status === 401 && window.location.pathname.indexOf('login') === -1) {
+    removeFromStorage('access-token')
+    window.location.reload()
+  }
+
+  return Promise.reject(error.response)
+}
+
+function httpClient(): AxiosInstance {
+  const defaultOptions = {
     baseURL: process.env.REACT_APP_API_URL || '',
-  })
+  }
+
+  const instance = axios.create(defaultOptions)
+
+  instance.interceptors.request.use(onRequest)
+  instance.interceptors.response.use(onResponseSuccess, onResponseError)
 
   return instance
 }
