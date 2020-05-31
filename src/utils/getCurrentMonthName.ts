22bdb1fec1d35730de85276e5655d37109d927e5
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-05-31 12:17:45 +0200

    Translate month names

--- src/utils/getCurrentMonthName.ts
+++ src/utils/getCurrentMonthName.ts
@@ -1,17 +1,18 @@
 export const getCurrentMonthName = (month: number) => {
+  const lang = localStorage.getItem('language')
   const months: Record<number, string> = {
-    1: 'Styczeń',
-    2: 'Luty',
-    3: 'Marzec',
-    4: 'Kwiecień',
-    5: 'Maj',
-    6: 'Czerwiec',
-    7: 'Lipiec',
-    8: 'Sierpień',
-    9: 'Wrzesień',
-    10: 'Październik',
-    11: 'Listopad',
-    12: 'Grudzień',
+    1: lang === 'pl' ? 'Styczeń' : 'January',
+    2: lang === 'pl' ? 'Luty' : 'February',
+    3: lang === 'pl' ? 'Marzec' : 'March',
+    4: lang === 'pl' ? 'Kwiecień' : 'April',
+    5: lang === 'pl' ? 'Maj' : 'May',
+    6: lang === 'pl' ? 'Czerwiec' : 'June',
+    7: lang === 'pl' ? 'Lipiec' : 'July',
+    8: lang === 'pl' ? 'Sierpień' : 'August',
+    9: lang === 'pl' ? 'Wrzesień' : 'September',
+    10: lang === 'pl' ? 'Październik' : 'October',
+    11: lang === 'pl' ? 'Listopad' : 'November',
+    12: lang === 'pl' ? 'Grudzień' : 'December',
   }
 
   return months[month]
